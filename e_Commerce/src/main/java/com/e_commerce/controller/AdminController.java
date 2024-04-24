991e4d1b2ec48953b52183a7ef108198bb3b2684
Shripatil200 <patilshriram200@gmail.com>
2024-04-24 20:31:36 +0530

    Using Thyleafe

--- e_Commerce/src/main/java/com/e_commerce/controller/AdminController.java
+++ e_Commerce/src/main/java/com/e_commerce/controller/AdminController.java
@@ -1,13 +1,18 @@
 package com.e_commerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
 	@GetMapping("/admin")
-	public String adminHome() {
+	public String adminHome(Model model) {
+		
+		String name="Shriram";
+		
+		model.addAttribute("name", name);
 		
 		return "adminHome";
 	}
